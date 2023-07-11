22784f66eb6872129dfe79725489f1de71ce9b04
Maryia <97767940+Masyak95@users.noreply.github.com>
2023-07-11 14:06:22 +0200

    add custom classNames

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './styles/index.scss';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {useTheme} from "./styles/theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 
 
@@ -13,7 +14,7 @@ const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames("app", {}, [theme])}>
             <button onClick={toggleTheme}>toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
