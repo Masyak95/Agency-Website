b2db76fa5b8f0067bd30e6562d75efe63e42606e
Maryia <97767940+Masyak95@users.noreply.github.com>
2023-07-19 13:51:51 +0200

    add BundleAnalyzer

--- src/app/providers/ErrorBoundary/ui/BugButton.tsx
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -1,13 +1,9 @@
-import { classNames } from 'shared/lib/classNames/classNames';
 import { Button } from 'shared/ui/Button/Button';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-interface BugButtonProps {
-    className?: string
-}
-
-export const BugButton = ({ className }: BugButtonProps) => {
+// test component
+export const BugButton = () => {
     const [error, setError] = useState(false);
     const { t } = useTranslation();
 
