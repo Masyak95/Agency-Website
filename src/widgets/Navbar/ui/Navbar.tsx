a6742ec7518e5c50b0a776f76b39fc4e6d73c765
Maryia <97767940+Masyak95@users.noreply.github.com>
2023-07-28 11:56:46 +0200

    update sidebar, fix test

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -9,21 +9,6 @@ interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => {
-    const { t } = useTranslation();
-
-    return (
-        <div className={classNames(cls.navbar, {}, [className])}>
-
-            <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
-                    {t('Main page')}
-                </AppLink>
-                <AppLink theme={AppLinkTheme.RED} to="/about">
-                    {t('About page')}
-                </AppLink>
-
-            </div>
-        </div>
-    );
-};
+export const Navbar = ({ className }: NavbarProps) => (
+    <div className={classNames(cls.navbar, {}, [className])} />
+);
