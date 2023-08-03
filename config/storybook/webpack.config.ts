decb9c3f577d932b53be0674f66d0b0b3451e466
Maryia <97767940+Masyak95@users.noreply.github.com>
2023-08-03 18:00:09 +0200

    update config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -28,8 +28,5 @@ export default ({ config }: {config: webpack.Configuration}) => {
     });
     config.module.rules.push(buildCssLoader(true));
 
-    config.resolve.alias = {
-        entities: path.resolve(__dirname, '..', '..', 'src', 'entities'),
-    };
     return config;
 };
